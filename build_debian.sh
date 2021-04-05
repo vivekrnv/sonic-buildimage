2149ef0533192aa69df292f90f89f8b6dade0c29
Vivek Reddy Karri <vkarri@nvidia.com>
2021-04-05 04:58:20 +0000

    Adding Missing Library
    Signed-off-by: Vivek Reddy Karri <vkarri@nvidia.com>

--- build_debian.sh
+++ build_debian.sh
@@ -314,6 +314,7 @@ sudo LANG=C DEBIAN_FRONTEND=noninteractive chroot $FILESYSTEM_ROOT apt-get -y in
     python3-distutils       \
     python3-pip             \
     cron                    \
+    libmnl0                 \
     haveged                 \
     jq
 
