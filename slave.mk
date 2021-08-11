c88529fcd7ca8a45abd5ecaf117d21ba0399179b
Vivek Reddy Karri <vkarri@nvidia.com>
2021-08-11 08:20:56 +0000

    auto TS changes
    Signed-off-by: Vivek Reddy Karri <vkarri@nvidia.com>

--- slave.mk
+++ slave.mk
@@ -935,6 +935,7 @@ $(addprefix $(TARGET_PATH)/, $(SONIC_INSTALLERS)) : $(TARGET_PATH)/% : \
 	export include_restapi="$(INCLUDE_RESTAPI)"
 	export include_nat="$(INCLUDE_NAT)"
 	export include_sflow="$(INCLUDE_SFLOW)"
+	export enable_auto_tech_support="$(ENABLE_AUTO_TECH_SUPPORT)"
 	export include_macsec="$(INCLUDE_MACSEC)"
 	export include_mgmt_framework="$(INCLUDE_MGMT_FRAMEWORK)"
 	export include_iccpd="$(INCLUDE_ICCPD)"
