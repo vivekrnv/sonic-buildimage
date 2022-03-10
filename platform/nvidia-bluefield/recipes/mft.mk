549dc7341a82ec754d5988bc8b5e914beb07a01f
Vivek Reddy Karri <vkarri@nvidia.com>
2022-03-10 03:15:09 +0000

    MFT version Upgraded
    Signed-off-by: Vivek Reddy Karri <vkarri@nvidia.com>

--- platform/nvidia-bluefield/recipes/mft.mk
+++ platform/nvidia-bluefield/recipes/mft.mk
@@ -14,8 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-MFT_VERSION = 4.18.0
-MFT_REVISION = 106
+MFT_VERSION = 4.18.1
+MFT_REVISION = 16
 
 export MFT_VERSION MFT_REVISION
 
