1c5931543b5eb7d468b345785f2c57f6a5b861a6
Vivek Reddy <vkarri@nvidia.com>
2022-08-17 06:49:21 +0000

    Added package to install perf
    Signed-off-by: Vivek Reddy <vkarri@nvidia.com>

--- build_debian.sh
+++ build_debian.sh
@@ -290,6 +290,7 @@ sudo LANG=C DEBIAN_FRONTEND=noninteractive chroot $FILESYSTEM_ROOT apt-get -y in
     sysfsutils              \
     squashfs-tools          \
     grub2-common            \
+    linux-tools             \
     rsyslog                 \
     ethtool                 \
     screen                  \
