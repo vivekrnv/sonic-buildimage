d993b681b90886e09fa1571a90da5822f944f66d
Vivek <vivekreddykarri98@gmail.com>
2022-09-12 02:51:36 -0700

    [GRUB] [Boot] Unified Grub config for all installer options (#39)
    Signed-off-by: Vivek Reddy Karri <vkarri@nvidia.com>

--- build_image.sh
+++ build_image.sh
@@ -113,82 +113,6 @@ clean_dir()
     exit $2
 }
 
-generate_s2s_installer_image_bluefield()
-{
-    if  [ ! -d installer ] || [ ! -r installer/sharch_body.sh ] ; then
-        echo "Error: Invalid installer script directory: installer"
-        exit 1
-    fi
-
-    if  [ ! -d installer/bluefield ] || [ ! -r installer/bluefield/sonic_to_sonic.sh ] ; then
-        echo "Error: Invalid arch installer directory: installer/bluefield"
-        exit 1
-    fi
-
-    # Copy platform-specific installer config files
-    rm -rf ./installer/bluefield/platforms/
-    mkdir -p ./installer/bluefield/platforms/
-    for PLATFORM in `ls ./device/nvidia-bluefield`; do
-        if [ -f ./device/nvidia-bluefield/$PLATFORM/installer.conf ]; then
-            cp ./device/nvidia-bluefield/$PLATFORM/installer.conf ./installer/bluefield/platforms/$PLATFORM
-        fi
-    done
-
-    echo -n "Building self-extracting install image ."
-    tmp_dir=$(mktemp --directory)
-    tmp_installdir="$tmp_dir/installer"
-    mkdir $tmp_installdir || clean_dir $tmp_dir 1
-
-    # Copy the bluefield specific sonic_to_sonic installer script and rename
-    cp installer/bluefield/sonic_to_sonic.sh $tmp_installdir/install.sh
-
-    # Replace the build time flags inside the script
-    sed -i -e "s/%%IMAGE_VERSION%%/$IMAGE_VERSION/g" \
-           -e "s/%%INSTALLER_PAYLOAD%%/$ONIE_INSTALLER_PAYLOAD/g" \
-           -e "s/%%FILESYSTEM_DOCKERFS%%/$FILESYSTEM_DOCKERFS/g" \
-           -e "s/%%DOCKERFS_DIR%%/$DOCKERFS_DIR/g" \
-           -e "s/%%FILESYSTEM_SQUASHFS%%/$FILESYSTEM_SQUASHFS/g" \
-           -e "s/%%KERNEL_VERSION%%/$KVERSION/g" $tmp_installdir/install.sh || clean_dir $tmp_dir 1
-    chmod 0755 $tmp_installdir/install.sh
-
-    # Copy the payload file
-    cp $ONIE_INSTALLER_PAYLOAD $tmp_installdir || clean_dir  $tmp_dir 1
-
-    # Create machine.conf file
-    echo "machine=nvidia-bluefield" > $tmp_installdir/machine.conf
-    echo "platform=arm64" >> $tmp_installdir/machine.conf
-
-    sharch="$tmp_dir/sharch.tar"
-    tar -C $tmp_dir -cf $sharch installer || {
-        echo "Error: Problems creating $sharch archive"
-        clean_dir $tmp_dir 1
-    }
-
-    [ -f "$sharch" ] || {
-        echo "Error: $sharch not found"
-        clean_dir $tmp_dir 1
-    }
-
-    sha1=$(cat $sharch | sha1sum | awk '{print $1}')
-    echo -n "."
-
-    cp installer/sharch_body.sh $OUTPUT_ONIE_IMAGE || {
-        echo "Error: Problems copying sharch_body.sh"
-        clean_dir 1
-    }
-
-    # Replace variables in the sharch template
-    sed -i -e "s/%%IMAGE_SHA1%%/$sha1/" $OUTPUT_ONIE_IMAGE
-    echo -n "."
-    cat $sharch >> $OUTPUT_ONIE_IMAGE
-    rm -rf $tmp_dir
-    echo " Done."
-
-    echo "Success:  Demo install image is ready in ${OUTPUT_ONIE_IMAGE}:"
-
-    clean_dir $tmp_dir  0
-}
-
 if [ "$IMAGE_TYPE" = "onie" ]; then
     echo "Build ONIE installer"
     mkdir -p `dirname $OUTPUT_ONIE_IMAGE`
@@ -299,19 +223,15 @@ elif [ "$IMAGE_TYPE" = "aboot" ]; then
         ./scripts/sign_image.sh -i "$OUTPUT_ABOOT_IMAGE" -k "$SIGNING_KEY" -c "$SIGNING_CERT" -a "$TARGET_CA_CERT"
     fi
 
-elif [[ $IMAGE_TYPE = s2s && $CONFIGURED_PLATFORM == nvidia-bluefield ]]; then
-    # bluefield Doesn't have ONiE Support yet and this *.bin image generated can only be used for SONiC-SONiC installation
-    echo "Build SONiC to SONiC installer for Bluefield"
-    mkdir -p `dirname $OUTPUT_ONIE_IMAGE`
-    sudo rm -f $OUTPUT_ONIE_IMAGE
-    generate_s2s_installer_image_bluefield
-
-elif [[ ( $IMAGE_TYPE == bfb || $IMAGE_TYPE == pxe ) && $CONFIGURED_PLATFORM == nvidia-bluefield ]]; then
+elif [[ $CONFIGURED_PLATFORM == nvidia-bluefield ]]; then
     sudo --preserve-env /sonic/installer/bluefield/create_sonic_image --kernel $KVERSION
-    sudo chown $USER ./$OUTPUT_BFB_IMAGE
-    if [[ $IMAGE_TYPE == pxe ]]; then
-        sudo chown $USER ./$OUTPUT_PXE_IMAGE
+    if [[ $IMAGE_TYPE != bin ]]; then
+        sudo chown $USER ./$OUTPUT_BFB_IMAGE
+        if [[ $IMAGE_TYPE == pxe ]]; then
+            sudo chown $USER ./$OUTPUT_PXE_IMAGE
+        fi
     fi
+
 else
     echo "Error: Non supported image type $IMAGE_TYPE"
     exit 1
