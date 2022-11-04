b2dcf777befdf1eae411a4aea58d3db4b44a44fe
Oleksandr Ivantsiv <oivantsiv@nvidia.com>
2022-11-04 13:14:01 +0200

    [bluefield] [SDK] submodule update.
    - Upgrade SDK to version 0.2-RC16

--- platform/nvidia-bluefield/recipes/sdk.mk
+++ platform/nvidia-bluefield/recipes/sdk.mk
@@ -19,7 +19,7 @@ SDK_BASE_PATH = $(PLATFORM_PATH)/sdk-src/sonic-bluefield-packages/bin
 
 # Place here URL where SDK sources exist
 SDK_SOURCE_BASE_URL =
-SDK_VERSION = 0.2-RC9
+SDK_VERSION = 0.2-RC16
 
 ifneq ($(SDK_SOURCE_BASE_URL), )
 SDK_FROM_SRC = y
