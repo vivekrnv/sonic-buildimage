0880950639603ff9981d93fc46d1e6e3e76cf5d1
Stepan Blyschak <stepanb@nvidia.com>
2022-11-09 17:28:43 +0000

    remove yang
    Signed-off-by: Stepan Blyschak <stepanb@nvidia.com>

--- src/sonic-yang-models/setup.py
+++ src/sonic-yang-models/setup.py
@@ -78,8 +78,6 @@ setup(
     cmdclass={'build_py': my_build_py},
     data_files=[
         ('yang-models', ['./yang-models/sonic-acl.yang',
-                         './yang-models/sonic-app-state-logging.yang',
-                         './yang-models/sonic-auto_techsupport.yang',
                          './yang-models/sonic-bgp-common.yang',
                          './yang-models/sonic-bgp-device-global.yang',
                          './yang-models/sonic-bgp-global.yang',
