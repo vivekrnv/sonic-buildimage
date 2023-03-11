387430789b35ef0fb77aa632c9901929b4a114c4
Vivek Reddy Karri <vkarri@nvidia.com>
2023-03-11 01:47:51 +0000

    Integrated hw-mgmt automation script into SONiC

--- platform/mellanox/hw-management.mk
+++ platform/mellanox/hw-management.mk
@@ -24,3 +24,84 @@ MLNX_HW_MANAGEMENT = hw-management_1.mlnx.$(MLNX_HW_MANAGEMENT_VERSION)_$(CONFIG
 $(MLNX_HW_MANAGEMENT)_SRC_PATH = $(PLATFORM_PATH)/hw-management
 $(MLNX_HW_MANAGEMENT)_DEPENDS += $(LINUX_HEADERS) $(LINUX_HEADERS_COMMON)
 SONIC_MAKE_DEBS += $(MLNX_HW_MANAGEMENT)
+
+TEMP_HW_MGMT_DIR = /tmp/hw_mgmt
+PTCH_DIR = $(TEMP_HW_MGMT_DIR)/patch_dir/
+NON_UP_PTCH_DIR = $(TEMP_HW_MGMT_DIR)/non_up_patch_dir/
+PTCH_LIST  = $(TEMP_HW_MGMT_DIR)/series
+KCFG_LIST = $(TEMP_HW_MGMT_DIR)/kconfig
+HWMGMT_NONUP_LIST = $(BUILD_WORKDIR)/$($(MLNX_HW_MANAGEMENT)_SRC_PATH)/hwmgmt_nonup_patches
+USER_OUTFILE = $(BUILD_WORKDIR)/integrate-mlnx-hw-mgmt_jenkins.out
+TMPFILE = /tmp/intg-hw-mgmt.out
+
+integrate-mlnx-hw-mgmt:
+	$(FLUSH_LOG)
+	rm -rf $(TEMP_HW_MGMT_DIR) $(TMPFILE)
+	mkdir -p $(PTCH_DIR) $(NON_UP_PTCH_DIR)
+	touch $(PTCH_LIST) $(KCFG_LIST)
+
+	# clean up existing untracked files
+	pushd $(BUILD_WORKDIR); git stash; git clean -f -- platform/mellanox/; popd
+	pushd $(BUILD_WORKDIR)/src/sonic-linux-kernel; git stash; git clean -f -- patch/; popd
+
+	echo "#### Integrate HW-MGMT ${MLNX_HW_MANAGEMENT_VERSION} Kernel Patches into SONiC" > ${USER_OUTFILE}
+	pushd $(BUILD_WORKDIR)/$(PLATFORM_PATH) $(LOG_SIMPLE)
+
+	# Run tests
+	pushd integration-scripts/tests; pytest-3 -v; popd
+
+	# Pre-processing before runing hw_mgmt script
+	integration-scripts/hwmgmt_kernel_patches.py pre \
+							--config_inclusion $(KCFG_LIST) \
+							--build_root $(BUILD_WORKDIR) $(LOG_SIMPLE)
+
+	$(BUILD_WORKDIR)/$($(MLNX_HW_MANAGEMENT)_SRC_PATH)/hw-mgmt/recipes-kernel/linux/deploy_kernel_patches.py \
+							--dst_accepted_folder $(PTCH_DIR) \
+							--dst_candidate_folder $(NON_UP_PTCH_DIR) \
+							--series_file $(PTCH_LIST) \
+							--config_file $(KCFG_LIST) \
+							--kernel_version $(KERNEL_VERSION) \
+							--os_type sonic $(LOG_SIMPLE)
+
+	# Post-processing
+	integration-scripts/hwmgmt_kernel_patches.py post \
+							--patches $(PTCH_DIR) \
+							--non_up_patches $(NON_UP_PTCH_DIR) \
+							--config_inclusion $(KCFG_LIST) \
+							--series $(PTCH_LIST) \
+							--current_non_up_patches $(HWMGMT_NONUP_LIST) \
+							--build_root $(BUILD_WORKDIR) $(LOG_SIMPLE)
+	
+	# Commit the changes in linux kernel and and log the diff
+	pushd $(BUILD_WORKDIR)/src/sonic-linux-kernel
+	git add -- patch/; git commit -m "Intgerate HW-MGMT ${MLNX_HW_MANAGEMENT_VERSION} Changes";
+
+	echo -en "\n###-> series file changes in sonic-linux-kernel <-###\n" >> ${USER_OUTFILE}
+	git diff --no-color  HEAD~1 HEAD -- patch/series >> ${USER_OUTFILE}
+
+	echo -en "\n###-> kconfig-inclusion file changes in sonic-linux-kernel <-###\n" >> ${USER_OUTFILE}
+	git diff --no-color  HEAD~1 HEAD -- patch/kconfig-inclusions >> ${USER_OUTFILE}
+
+	echo -en '\n###-> Summary of files updated in sonic-linux-kernel <-###\n' >> ${USER_OUTFILE}
+	git diff --no-color  HEAD~1 HEAD --stat --output=${TMPFILE}
+	cat ${TMPFILE} | tee -a ${USER_OUTFILE}
+	popd
+
+	# Commit the changes in buildimage and log the diff
+	pushd $(BUILD_WORKDIR)
+	git add -- $(PLATFORM_PATH); git commit -m "Intgerate HW-MGMT ${MLNX_HW_MANAGEMENT_VERSION} Changes";
+
+	echo -en '\n###-> Non Upstream series.patch changes <-###\n' >> ${USER_OUTFILE}
+	git diff --no-color  HEAD~1 HEAD -- $(PLATFORM_PATH)/non-upstream-patches/series.patch >> ${USER_OUTFILE}
+
+	echo -en '\n###-> Non Upstream patch list file <-###\n' >> ${USER_OUTFILE}
+	git diff --no-color  HEAD~1 HEAD -- $($(MLNX_HW_MANAGEMENT)_SRC_PATH)/hwmgmt_nonup_patches >> ${USER_OUTFILE}
+	
+	echo -en '\n###-> Summary of buildimage changes <-###\n' >> ${USER_OUTFILE}
+	git diff --no-color  HEAD~1 HEAD --stat --output=${TMPFILE} -- $(PLATFORM_PATH)
+	cat ${TMPFILE} | tee -a ${USER_OUTFILE}
+	popd
+
+	popd $(LOG_SIMPLE)
+
+SONIC_PHONY_TARGETS += integrate-mlnx-hw-mgmt
