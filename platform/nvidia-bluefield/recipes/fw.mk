ed232ef40b8bb3aaf11dd0d65ce805b0ff6c3217
Oleksandr Ivantsiv <oivantsiv@nvidia.com>
2023-05-23 19:05:18 +0300

    [bluefield][SDK][FW] Update FW version in makefile.
    - Upgrade FW version to v32.37.1300

--- platform/nvidia-bluefield/recipes/fw.mk
+++ platform/nvidia-bluefield/recipes/fw.mk
@@ -18,7 +18,7 @@
 FW_BASE_PATH = $(PLATFORM_PATH)/sdk-src/sonic-bluefield-packages/fw
 BF3_FW_BASE_URL =
 
-BF3_FW_VERSION = 32.37.0242
+BF3_FW_VERSION = 32.37.1300
 
 BF3_FW_FILE = fw-BlueField-3-rel-$(subst .,_,$(BF3_FW_VERSION)).mfa
 $(BF3_FW_FILE)_PATH = $(FW_BASE_PATH)/
