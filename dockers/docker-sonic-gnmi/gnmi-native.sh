3c47c88558572b0598b19d90fdf91998f85cb88b
Vivek Reddy <vkarri@nvidia.com>
2023-12-13 22:14:52 +0200

    Enable zmq_address option on gnmi for standalone dpu
    Signed-off-by: Vivek Reddy <vkarri@nvidia.com>

--- dockers/docker-sonic-gnmi/gnmi-native.sh
+++ dockers/docker-sonic-gnmi/gnmi-native.sh
@@ -71,10 +71,10 @@ else
 fi
 
 # Enable ZMQ for SmartSwitch
-LOCALHOST_SUBTYPE=`sonic-db-cli CONFIG_DB hget localhost "subtype"`
-if [[ x"${LOCALHOST_SUBTYPE}" == x"SmartSwitch" ]]; then
-    TELEMETRY_ARGS+=" -zmq_address=tcp://127.0.0.1:8100"
-fi
+# LOCALHOST_SUBTYPE=`sonic-db-cli CONFIG_DB hget localhost "subtype"`
+# if [[ x"${LOCALHOST_SUBTYPE}" == x"SmartSwitch" ]]; then
+TELEMETRY_ARGS+=" -zmq_address=tcp://127.0.0.1:8100"
+# fi
 
 # Server will handle threshold connections consecutively
 THRESHOLD_CONNECTIONS=$(echo $GNMI | jq -r '.threshold')
