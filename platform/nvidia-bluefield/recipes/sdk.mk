b4da2e9368489e2b2e8f1dde78c0c13c20f9148b
Yakiv Huryk <yhuryk@nvidia.com>
2024-01-15 16:16:59 +0200

    [bluefield] update SDK to 23.10-RC9, SAI to SAIBuild0.0.27.1

--- platform/nvidia-bluefield/recipes/sdk.mk
+++ platform/nvidia-bluefield/recipes/sdk.mk
@@ -21,7 +21,7 @@ SDK_BASE_PATH = $(PLATFORM_PATH)/sdk-src/sonic-bluefield-packages/bin
 
 # Place here URL where SDK sources exist
 SDK_SOURCE_BASE_URL =
-SDK_VERSION = 23.10-RC8
+SDK_VERSION = 23.10-RC9
 
 SDK_COLLECTX_URL = https://linux.mellanox.com/public/repo/doca/1.5.2/debian11/aarch64/
 
