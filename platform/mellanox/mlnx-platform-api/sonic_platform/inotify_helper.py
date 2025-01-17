6d945df80b0b4ddc50b7d60d435877d0788c0bc7
Vivek <vivekreddykarri98@gmail.com>
2025-01-17 10:14:17 -0800

    Fix the header check failure

--- platform/mellanox/mlnx-platform-api/sonic_platform/inotify_helper.py
+++ platform/mellanox/mlnx-platform-api/sonic_platform/inotify_helper.py
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES.
+# Copyright (c) 2024-2025 NVIDIA CORPORATION & AFFILIATES.
 # Apache-2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
