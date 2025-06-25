0f297b01af8c2fde942f6af1283d7c8f1b5f4f0b
Vivek <vivekreddykarri98@gmail.com>
2025-06-25 09:52:33 -0700

    Update copyright headers

--- platform/nvidia-bluefield/recipes/fw.mk
+++ platform/nvidia-bluefield/recipes/fw.mk
@@ -1,6 +1,5 @@
 #
-# SPDX-FileCopyrightText: NVIDIA CORPORATION & AFFILIATES
-# Copyright (c) 2024-2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# Copyright (c) 2024-2025 NVIDIA CORPORATION & AFFILIATES.
 # Apache-2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
