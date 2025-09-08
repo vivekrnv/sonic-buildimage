d02fc0334c34b1047c182a1f0735ba6043d0839b
Saikrishna Arcot <sarcot@microsoft.com>
2025-09-07 18:49:33 -0700

    Start updating the build rules for platform modules and fix some of the kernel module compilation errors
    Signed-off-by: Saikrishna Arcot <sarcot@microsoft.com>

--- platform/broadcom/rules.mk
+++ platform/broadcom/rules.mk
@@ -11,11 +11,11 @@ include $(PLATFORM_PATH)/platform-modules-arista.mk
 include $(PLATFORM_PATH)/platform-modules-nexthop.mk
 #include $(PLATFORM_PATH)/platform-modules-ingrasys.mk
 include $(PLATFORM_PATH)/platform-modules-accton.mk
-include $(PLATFORM_PATH)/platform-modules-alphanetworks.mk
+#include $(PLATFORM_PATH)/platform-modules-alphanetworks.mk
 #include $(PLATFORM_PATH)/platform-modules-inventec.mk
 include $(PLATFORM_PATH)/platform-modules-cel.mk
 #include $(PLATFORM_PATH)/platform-modules-delta.mk
-include $(PLATFORM_PATH)/platform-modules-quanta.mk
+#include $(PLATFORM_PATH)/platform-modules-quanta.mk
 ##include $(PLATFORM_PATH)/platform-modules-mitac.mk
 #include $(PLATFORM_PATH)/platform-modules-juniper.mk
 #include $(PLATFORM_PATH)/platform-modules-brcm-xlr-gts.mk
