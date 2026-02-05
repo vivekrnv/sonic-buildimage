ec26cca83a09bdaa91148435d76e9a4ca2942c3d
Vivek Reddy <vkarri@nvidia.com>
2026-02-05 07:43:30 +0000

    [Trixie] Add hwclock binary
    Signed-off-by: Vivek Reddy <vkarri@nvidia.com>

--- build_debian.sh
+++ build_debian.sh
@@ -383,7 +383,8 @@ sudo LANG=C DEBIAN_FRONTEND=noninteractive chroot $FILESYSTEM_ROOT apt-get -y in
     wireless-regdb          \
     ethtool                 \
     zstd                    \
-    nvme-cli
+    nvme-cli                \
+    util-linux-extra
 
 sudo cp files/initramfs-tools/pzstd $FILESYSTEM_ROOT/etc/initramfs-tools/hooks/pzstd
 sudo chmod +x $FILESYSTEM_ROOT/etc/initramfs-tools/hooks/pzstd
